e99e7d2bb8514f63f34cf07931fb7f2cb0555907
Luiz Felipe <luizfellipe8533@gmail.com>
2024-10-27 19:47:02 -0300

    testing format

--- lib/api_client.ex
+++ lib/api_client.ex
@@ -8,7 +8,8 @@ defmodule RiotDeveloperClient.ApiClient do
       {Tesla.Middleware.JSON, engine: Jason, engine_opts: [keys: :atoms]}
     ]
 
-    Tesla.client(middleware, adapter())
+    Tesla.client(middleware, 
+    adapter())
   end
 
   defp adapter do
